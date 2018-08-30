8635adea1ec9e1cc8a6a61f7551676d4dc487c5c
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-08-29 22:15:35 -0300

    Alura orientação objeto

---
+++ ByteBank/1-ByteBank/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    class ContaCorrente
+    {
+        public string nome;
+        public int agencia;
+        public int conta;
+        public double saldo;
+
+    }
+}
---
+++ ByteBank/1-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente minhaConta = new ContaCorrente();
+            minhaConta.saldo = 500;
+
+            ContaCorrente outraConta = minhaConta;
+            outraConta.saldo += 1000;
+
+            Console.WriteLine(minhaConta.saldo);
+            Console.ReadLine();
+
+        }
+    }
+}
